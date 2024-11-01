826186568e092f254a545055e8bc67a5c9520df1
Vivekanand <vkhiremath368@gmail.com>
2024-11-01 12:22:09 +0530

    Update dag.py

--- dag.py
+++ dag.py
@@ -12,3 +12,11 @@ from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 s3_bucket = Variable.get("S3_BUCKET")
 data_source_url = Variable.get("DATA_SOURCE_URL")
+
+'''
+This code is doing the following:
+
+Importing modules for working with temporary files, accessing AWS, making web requests and parsing JSON
+Importing modules required for defining a DAG and its components
+Creating two Apache Airflow variables for the S3 bucket name and the data source URL
+'''
